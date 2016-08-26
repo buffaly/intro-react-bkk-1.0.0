1c1b843b4bc435afde2e200f24290b15c293865a
buffaly <buffalo660@gmail.com>
2016-08-27 02:11:51 +0700

    event add to-do-list

--- src/Todo.js
+++ src/Todo.js
@@ -4,32 +4,51 @@ import './Todo.css';
 
 export class Todo extends Component {
 
+  state = {
+    inputText: '',
+    listItem: []
+  }
+
+  handleChangeText = (event) => {
+    this.setState({inputText: event.target.value});
+  }
+
+  submitList = () => {
+    this.setState({ 
+      listItem: this.state.listItem.concat([this.state.inputText]),
+      inputText: ''
+    })
+  }
+
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.submitList();
+    }
+  }
+
   render() {
     return (
       <div className="card clearfix">
           <div className="to-do-box">
               <h1 className="title">To-do-list</h1>
               <div className="form-box">
-                  <input className="input-todo" placeholder="what do you want to do?" type="text"/>
-                  <button className="bb-input button">Add</button>
+                  <input className="input-todo"
+                         type="text"
+                         onChange={this.handleChangeText}
+                         value={this.state.inputText}
+                         placeholder="what do you want to do?"
+                         onKeyPress={this.handleKeyPress}/>
+                  <button className="bb-input button"
+                          onClick={this.submitList}>Add</button>
               </div>
-
-              <div className="list-box">
-                  <div className="list-item">
-                      <div className="text-list">To do item 1</div>
-                      <div className="bb-action">x</div>
-                  </div>
-                  <div className="list-item">
-                      <div className="text-list">To do item 2</div>
-                      <div className="bb-action">x</div>
-                  </div>
-              </div>
-
+              {
+                this.state.listItem.map((value, index) => {
+                  return (
+                    <h3 key={index + value}>{value}</h3>
+                  );
+                })
+              }
           </div>
-
-          <div className="tab-page">Page 1</div>
-          <div className="second tab-page">Page 2</div>
-          <div className="tab-page">Page 3</div>
       </div>
     );
   }
