9be0aeb86bb6c0fa2ae730869748904fe25aff0f
buffaly <buffalo660@gmail.com>
2016-08-27 00:57:00 +0700

    create Todo.js with create new component

---
+++ src/Todo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Todo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>New component</h1>
+      </div>
+    );
+  }
+}
